fbe28579d7d1da511757b4bbdf27c49f41b5aaa9
hector <hectorumaga@gmail.com>
2019-04-02 14:28:08 -0300

    l

--- controllers/listController.js
+++ controllers/listController.js
@@ -370,7 +370,7 @@ exports.gerarExcel = function (req, res) {
         body += "<data>" + moment(row.data_venda).format('MM/DD/YYYY') + "</data>"
         body += "<alimento>" + row.alimento + "</alimento>"
         body += "<sexo>" + row.sexo + "</sexo>"
-        body += "<comboo>" + row.combo + "</combo>"
+        body += "<combo>" + row.combo + "</combo>"
         body += "</tr>"
       })
       var html = "<?xml version='1.0' encoding='UTF-8'?>";
